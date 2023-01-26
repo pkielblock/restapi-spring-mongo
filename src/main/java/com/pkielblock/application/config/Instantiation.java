4fc6d5c631c9676c9332f3fef35b191d73da713c
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-26 14:46:11 -0300

    feeding database

---
+++ src/main/java/com/pkielblock/application/config/Instantiation.java
@@ -0,0 +1,26 @@
+package com.pkielblock.application.config;
+
+import com.pkielblock.application.domain.User;
+import com.pkielblock.application.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
--- src/main/java/com/pkielblock/application/resources/UserResources.java
+++ src/main/java/com/pkielblock/application/resources/UserResources.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
