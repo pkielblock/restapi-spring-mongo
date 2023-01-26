96291d8ed447c9f0912573728f75749554d029bc
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-26 16:17:54 -0300

    user delete

--- src/main/java/com/pkielblock/application/resources/UserResources.java
+++ src/main/java/com/pkielblock/application/resources/UserResources.java
@@ -42,4 +42,10 @@ public class UserResources {
 
         return ResponseEntity.created(uri).build();
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
