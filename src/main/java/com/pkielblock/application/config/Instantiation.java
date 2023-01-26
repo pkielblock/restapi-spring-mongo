e1512987e0a0113b9f5f0eea7226a601910a4542
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-26 17:15:40 -0300

    adding comments to posts

--- src/main/java/com/pkielblock/application/config/Instantiation.java
+++ src/main/java/com/pkielblock/application/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.pkielblock.application.config;
 import com.pkielblock.application.domain.Post;
 import com.pkielblock.application.domain.User;
 import com.pkielblock.application.dto.AuthorDTO;
+import com.pkielblock.application.dto.CommentDTO;
 import com.pkielblock.application.repositories.PostRepository;
 import com.pkielblock.application.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,6 +38,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para SP abraços", new AuthorDTO(maria));
         Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acorderi feliz hoje", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um otimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        p1.getComments().addAll(Arrays.asList(c1, c2));
+        p2.getComments().add(c3);
 
         postRepository.saveAll(Arrays.asList(p1, p2));
 
--- src/main/java/com/pkielblock/application/domain/Post.java
+++ src/main/java/com/pkielblock/application/domain/Post.java
@@ -1,32 +1,36 @@
 package com.pkielblock.application.domain;
 
 import com.pkielblock.application.dto.AuthorDTO;
+import com.pkielblock.application.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "post")
 public class Post implements Serializable {
     @Id
-    public String id;
-    public Date date;
-    public String title;
-    public String body;
-    public AuthorDTO authorDTO;
+    private String id;
+    private Date date;
+    private String title;
+    private String body;
+    private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
 
     }
 
-    public Post(String id, Date date, String title, String body, AuthorDTO authorDTO) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
         this.body = body;
-        this.authorDTO = authorDTO;
+        this.author = author;
     }
 
     public String getId() {
@@ -61,12 +65,16 @@ public class Post implements Serializable {
         this.body = body;
     }
 
-    public AuthorDTO getAuthorDTO() {
-        return authorDTO;
+    public AuthorDTO getAuthor() {
+        return author;
     }
 
-    public void setAuthorDTO(AuthorDTO authorDTO) {
-        this.authorDTO = authorDTO;
+    public void setAuthor(AuthorDTO authorDTO) {
+        this.author = authorDTO;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
     }
 
     @Override
