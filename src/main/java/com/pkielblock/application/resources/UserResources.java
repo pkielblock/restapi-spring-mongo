f5771a0c80f21c4aefe81298bac1a9801d21dc32
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-26 14:19:45 -0300

    User Entity and Rest Working

---
+++ src/main/java/com/pkielblock/application/resources/UserResources.java
@@ -0,0 +1,27 @@
+package com.pkielblock.application.resources;
+
+import com.pkielblock.application.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        User maria = new User("1", "Maria Brown", "maria@gmail.com");
+        User alex = new User("2", "Alex Green", "alex@gmail.com");
+
+        List<User> list = new ArrayList<>(Arrays.asList(maria, alex));
+
+        return ResponseEntity.ok().body(list);
+    }
+}
