13e9b2d88a66d1fef5074529e63a246d48d14f70
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-26 12:59:08 -0300

    Initial Commit

---
+++ src/main/java/com/pkielblock/application/RestapiSpringMongoApplication.java
@@ -0,0 +1,13 @@
+package com.pkielblock.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiSpringMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestapiSpringMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pkielblock/application/RestapiSpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkielblock.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiSpringMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
