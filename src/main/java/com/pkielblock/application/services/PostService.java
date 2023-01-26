9d907380111c5c395b64fb57a158b1a9409bd555
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-26 17:38:48 -0300

    simple query with @Query

--- src/main/java/com/pkielblock/application/services/PostService.java
+++ src/main/java/com/pkielblock/application/services/PostService.java
@@ -20,6 +20,6 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String text) {
-        return repository.findByTitleContainingIgnoreCase(text);
+        return repository.searchTitle(text);
     }
 }
