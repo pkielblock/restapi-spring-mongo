0b65091088b1f93de18830d84cdcdf0c7c502e91
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-26 17:05:12 -0300

    endpoint retrieving post by id

---
+++ src/main/java/com/pkielblock/application/resources/PostResource.java
@@ -0,0 +1,23 @@
+package com.pkielblock.application.resources;
+
+import com.pkielblock.application.domain.Post;
+import com.pkielblock.application.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+    @Autowired
+    private PostService service;
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post post = service.findById(id);
+
+        return ResponseEntity.ok().body(post);
+    }
+}
---
+++ src/main/java/com/pkielblock/application/services/PostService.java
@@ -0,0 +1,20 @@
+package com.pkielblock.application.services;
+
+import com.pkielblock.application.domain.Post;
+import com.pkielblock.application.domain.User;
+import com.pkielblock.application.repositories.PostRepository;
+import com.pkielblock.application.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repository;
+    public Post findById(String id) {
+        Optional<Post> post = repository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Object Not Found"));
+    }
+}
